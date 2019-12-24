25396156e2cf7ff7541c3b71074e79e7a5717990
Jesse Jorgenson <jljorgenson18@gmail.com>
2019-12-23 21:40:10 -0600

    Clean up

--- src/math/common.ts
+++ src/math/common.ts
@@ -1,39 +1,37 @@
-import model1 from "./model1";
-
-export interface modelState {
-    t?: number, // Time
-    s?: any[] // Species
-    // r?: any[] // Resource pool
+export interface ModelState {
+  t?: number; // Time
+  s?: any[]; // Species
+  // r?: any[] // Resource pool
 }
 
 // Creating and modifying the state
 
-export function createInitialState(N: any[]): modelState {
-    let state: modelState = {t:0, s:N};
-    return state;
+export function createInitialState(N: any[]): ModelState {
+  const state: ModelState = { t: 0, s: N };
+  return state;
 }
 
-export function advanceTime(dt: number, initialState: modelState ): modelState {
-    let finalState: modelState = {t: initialState.t + dt, s: initialState.s}
-    return finalState;
+export function advanceTime(dt: number, initialState: ModelState): ModelState {
+  const finalState: ModelState = { t: initialState.t + dt, s: initialState.s };
+  return finalState;
 }
 
-export function createSpecies(initialState: modelState, newVal: any): modelState {
-    let finalState: modelState = initialState;
-    finalState.s.push(newVal)
-    return finalState
+export function createSpecies(initialState: ModelState, newVal: any): ModelState {
+  const finalState: ModelState = initialState;
+  finalState.s.push(newVal);
+  return finalState;
 }
 
-export function removeSpecies(initialState: modelState, index: number): modelState {
-    let finalState: modelState = {
-        t: initialState.t, 
-        s: initialState.s.filter((spec,ind) => ind != index)
-    }
-    return finalState
+export function removeSpecies(initialState: ModelState, index: number): ModelState {
+  const finalState: ModelState = {
+    t: initialState.t,
+    s: initialState.s.filter((spec, ind) => ind != index)
+  };
+  return finalState;
 }
 
-export function changeSpecies(initialState: modelState, index: number, newVal: any): modelState {
-    let finalState: modelState = initialState
-    finalState.s[index] = newVal
-    return finalState
+export function changeSpecies(initialState: ModelState, index: number, newVal: any): ModelState {
+  const finalState: ModelState = initialState;
+  finalState.s[index] = newVal;
+  return finalState;
 }
