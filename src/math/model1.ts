fcf8b1b4a88abb294b0bdffbc8467bf195881ed5
Jesse Jorgenson <jljorgenson18@gmail.com>
2019-12-23 18:22:36 -0600

    Pushing up model test code

---
+++ src/math/model1.ts
@@ -0,0 +1,19 @@
+import { SamplePayload } from 'src/db/sample';
+
+// Test code, definitely doesn't work... yet
+interface ModelState {
+  x: number;
+  y: number;
+}
+export default function model1(payload: SamplePayload): any {
+  const { name, model } = payload;
+  let state = {
+    x: 0,
+    y: 0
+  };
+  state = newState(state);
+}
+
+function newState(state: ModelState): ModelState {
+  return state;
+}
