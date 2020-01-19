9476cacd174368ad5f8f63a9fe5582993470e264
Jesse Jorgenson <jljorgenson18@gmail.com>
2020-01-18 18:45:38 -0600

    Adding a way to see initial conditions

--- src/components/SampleList.tsx
+++ src/components/SampleList.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Heading, Box, Text, Menu, DataTable } from 'grommet';
-
+import styled from 'styled-components';
 import { SampleDoc } from 'src/db/sample';
 
 interface SampleListProps {
   samples: SampleDoc[];
   onDeleteSample: (sample: SampleDoc) => void;
 }
+
+const InitialConditions = styled.pre`
+  border: 1px solid #aaa;
+  padding: 8px;
+  margin: 0;
+`;
+
+const initialConditionFields = ['a', 'b', 'ka', 'kb', 'kn', 'na', 'nb', 'nb'];
 
 function SampleList(props: SampleListProps): JSX.Element {
   const { samples, onDeleteSample } = props;
@@ -27,16 +35,29 @@ function SampleList(props: SampleListProps): JSX.Element {
           header: <Text>Name</Text>
         },
         {
+          property: 'initial conditions',
+          header: <Text>Initial Conditions</Text>,
+          render(sample: SampleDoc) {
+            const intialConditions = initialConditionFields.reduce<any>((ic, field) => {
+              if ((sample as any)[field] != null) ic[field] = (sample as any)[field];
+              return ic;
+            }, {});
+            return (
+              <InitialConditions>{JSON.stringify(intialConditions, null, '  ')}</InitialConditions>
+            );
+          }
+        },
+        {
           property: 'createdAt',
           header: <Text>Created At</Text>,
-          render(sample) {
+          render(sample: SampleDoc) {
             return <Text>{new Date(sample.createdAt).toLocaleString()}</Text>;
           }
         },
         {
           property: 'actions',
           header: <Text>Actions</Text>,
-          render(sample) {
+          render(sample: SampleDoc) {
             return (
               <Menu
                 label="Actions"
