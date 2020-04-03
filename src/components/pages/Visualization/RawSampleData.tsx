153a66a6ca2602956390b9e58437b4390152592c
Jesse Jorgenson <jljorgenson18@gmail.com>
2020-04-03 00:11:07 -0500

    Cleaning up RawSampleData viewer

--- src/components/pages/Visualization/RawSampleData.tsx
+++ src/components/pages/Visualization/RawSampleData.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { VizProps } from './types';
 import ReactJson from 'react-json-view';
+import { VizProps } from './types';
+import { SampleDoc, SampleData } from 'src/db/sample';
+
+const getJSONSrc = (sample: SampleDoc, data: SampleData) => {
+  const { _id, createdAt, _rev, _attachments, ...rest } = sample;
+  const src = { _id, createdAt, ...rest, data };
+  return src;
+};
 
 function RawSampleData(props: VizProps) {
-  const { sample } = props;
-  return <ReactJson src={sample} collapsed={2} theme="solarized" enableClipboard={false} />;
+  const { sample, data } = props;
+  return (
+    <ReactJson
+      src={getJSONSrc(sample, data)}
+      collapsed={2}
+      theme="solarized"
+      displayDataTypes={false}
+      enableClipboard={false}
+    />
+  );
 }
 
 export default RawSampleData;
