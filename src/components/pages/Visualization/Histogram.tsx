97f635a0cbed34c6f1031fd37806b801bdc9a761
Jesse Jorgenson <jljorgenson18@gmail.com>
2020-04-03 00:32:21 -0500

    Cleaning up histogram and improving the yDomain

--- src/components/pages/Visualization/Histogram.tsx
+++ src/components/pages/Visualization/Histogram.tsx
@@ -1,10 +1,38 @@
-import React, { useState, useRef } from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import React, { useState, useRef, useMemo } from 'react';
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+  AxisDomain
+} from 'recharts';
 import { VizProps } from './types';
 import { RangeInput, Box } from 'grommet';
 import SaveChart from './common/SaveChart';
 import Controls from './common/Controls';
+import { DataPoint } from 'src/math/analysis';
+import { SampleData } from 'src/db/sample';
 
+function useStableYDomain(data: SampleData): [AxisDomain, AxisDomain] {
+  const highestPValue = useMemo(() => {
+    const highestPFromDataPoints = (dataPoints: DataPoint[]): number => {
+      return dataPoints.reduce((current, dataPoint) => {
+        const highestP = dataPoint.p;
+        if (highestP > current) return highestP;
+        return current;
+      }, -Infinity);
+    };
+    return data.histograms.reduce((current, histogram) => {
+      const highestP = highestPFromDataPoints(histogram.h);
+      if (highestP > current) return highestP;
+      return current;
+    }, -Infinity);
+  }, [data.histograms]);
+  return [0, Math.round(highestPValue)];
+}
 function Histogram(props: VizProps) {
   const {
     sample: { name },
@@ -16,14 +44,15 @@ function Histogram(props: VizProps) {
   // Each key contains t and the histogram. User should select which histogram to use
   // based on the t value.
   const plot_data = data.histograms[selectedBin].h;
+  const yDomain = useStableYDomain(data);
   // LINES should be generated based on one or more selected values from keys
   return (
     <>
-      <ResponsiveContainer minHeight={300} width="100%">
+      <ResponsiveContainer height={350} width="100%">
         <BarChart data={plot_data} ref={chartRef}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="t" name="Size" />
-          <YAxis />
+          <YAxis domain={yDomain} />
           <Tooltip labelFormatter={(size: number) => `Size: ${size}`} />
           <Bar dataKey="p" fill="#8884d8" />
         </BarChart>
